11e88f64ed58b23c54897235a93cab97d37d21eb
Pinninti Anusha <pinnintianusha756@gmail.com>
2025-02-01 09:37:50 +0530

    commit to dev

--- wipro_java_project/src/com/wipro/java/HelloWorld.java
+++ wipro_java_project/src/com/wipro/java/HelloWorld.java
@@ -19,7 +19,7 @@ public class HelloWorld {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("My First Java Project");
+		System.out.println("My First Java Project.....");
 
 	}
 
