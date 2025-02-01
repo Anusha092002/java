9b25a3a50a69cc6f01caefa97ba05f6a495eb986
Pinninti Anusha <pinnintianusha756@gmail.com>
2025-02-01 17:35:16 +0530

    commit to dev

--- wipro_java_project/src/com/wipro/java/oops/Main.java
+++ wipro_java_project/src/com/wipro/java/oops/Main.java
@@ -6,21 +6,19 @@ package com.wipro.java.oops;
 /**
  * 
  */
-class MyClass{
-	
-	int num=5;
-	public void changeValue(int num) {
-        this.num = num;
-    }
-}
+
  public class Main {
 	public  Main(){
 		 
 	 }
+	int num=5;
+	public void changeValue(int num) {
+        this.num = num;
+    }
 
 	 public static void main(String[] args) {
-	        MyClass obj = new MyClass();
-	        obj.changeValue(10);
+		 Main obj = new Main();// creating object
+	        obj.changeValue(10);// calling method changeValue
 	        System.out.println(obj.num);// Output will be 10
 	    }
 
