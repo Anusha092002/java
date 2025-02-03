5675682380527871981541d334a1e5c5f0111799
Pinninti Anusha <pinnintianusha756@gmail.com>
2025-02-03 12:18:30 +0530

    commit to dev

--- wipro_java_project/src/com/wipro/java/oops/Manager.java
+++ wipro_java_project/src/com/wipro/java/oops/Manager.java
@@ -9,5 +9,18 @@ package com.wipro.java.oops;
  * extends is an keyword to inherit Parent class from Child class
  */
 public class Manager extends Employee {
-
+	//constructor
+    public Manager() {
+    	
+    }
+    public static void main(String a[]) {
+    	
+    	Employee manager=new Manager();
+    	manager.setEmployee_id(122);
+    	manager.setEmployee_name("Anusha");
+    	manager.setEmpolyee_email("anusha@123gmail.com");
+    	manager.setEmployee_department("Management");
+    	System.out.println("Employee Id: "+manager.getEmployee_id()+"\nEmployee Name: "+manager.getEmployee_name()+"\nEmployee Email: "+manager.getEmpolyee_email()+"\nEmployee Department: "+manager.getEmployee_department());
+    	
+    }
 }
--- wipro_java_project/src/com/wipro/java/oops/ProjectLead.java
+++ wipro_java_project/src/com/wipro/java/oops/ProjectLead.java
@@ -8,6 +8,20 @@ package com.wipro.java.oops;
  * Child class= Manager
  * extends is an keyword to inherit Parent class from Child class
  */
-public class ProjectLead {
-
+public class ProjectLead extends Employee{
+	//Constructor
+    public ProjectLead() {
+    	
+    }
+    public static void main(String a[]) {
+    	// child object is instantiated from child constructor
+    	//parent class consumes the prop and behaviours of child class
+    	Employee employee=new ProjectLead();
+    	employee.setEmployee_id(120);
+    	employee.setEmployee_name("Anusha");
+    	employee.setEmpolyee_email("anusha@123gmail.com");
+    	employee.setEmployee_department("Project Guide");
+    	System.out.println("Employee Id: "+employee.getEmployee_id()+"\nEmployee Name: "+employee.getEmployee_name()+"\nEmployee Email: "+employee.getEmpolyee_email()+"\nEmployee Department: "+employee.getEmployee_department());
+    	
+    }
 }
