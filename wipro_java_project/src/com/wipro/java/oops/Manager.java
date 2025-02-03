70b3553ecccae84fc342d15dba433e007d22dfdd
Pinninti Anusha <pinnintianusha756@gmail.com>
2025-02-03 11:08:17 +0530

    commit to dev

---
+++ wipro_java_project/src/com/wipro/java/oops/Manager.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+package com.wipro.java.oops;
+
+/**
+ * Parent class= Employee
+ * Child class= Manager
+ * extends is an keyword to inherit Parent class from Child class
+ */
+public class Manager extends Employee {
+
+}
