aff510c585f21bbf4e48c6657709a04885b93fa9
Pinninti Anusha <pinnintianusha756@gmail.com>
2025-02-04 10:56:47 +0530

    commit to dev

---
+++ wipro_java_project/src/com/wipro/java/evaluation/SBI.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+package com.wipro.java.evaluation;
+
+/**
+ * This is Child calss
+ */
+public class SBI extends Bank {
+	private float balance;
+	public SBI(float initialBalance) {
+        this.balance = initialBalance;
+    }
+	float rateOfInterest=9f;
+	public void deposit(float amount) {
+        if(amount > 0) {
+            balance += amount;
+            System.out.println("Deposited: $" + amount);
+        } else {
+            System.out.println("Invalid amount.");
+        }
+    }
+
+    // Overloaded method to deposit money (with two parameters - deposit amount and deposit reason)
+    public void deposit(float amount, String reason) {
+        if(amount > 0) {
+            balance += amount;
+            System.out.println("Deposited: $" + amount + " for reason: " + reason);
+        } else {
+            System.out.println("Invalid amount.");
+        }
+    }
+	public void rateOfInterest() {
+		System.out.println("SBI Rate of Interest is:"+rateOfInterest);
+	}
+
+}
