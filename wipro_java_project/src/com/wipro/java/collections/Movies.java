7e0a96913b3ce8945b2890f8378c4cad99b87dd0
Pinninti Anusha <pinnintianusha756@gmail.com>
2025-02-06 11:25:52 +0530

    commit to dev

--- wipro_java_project/src/com/wipro/java/collections/Movies.java
+++ wipro_java_project/src/com/wipro/java/collections/Movies.java
@@ -10,7 +10,7 @@ import java.util.Collections;
 import java.util.List;
 import java.util.ArrayList;
 
-public class Movies implements Comparable<Movies> {
+public class Movies  {
     private String name;
     private double rating;
     private int year;
@@ -42,27 +42,6 @@ public class Movies implements Comparable<Movies> {
         return "Movie: " + name + ", Rating: " + rating + ", Year: " + year;
     }
 
-    // Implement compareTo to compare movies by rating
-    @Override
-    public int compareTo(Movies other) {
-        return Double.compare(this.rating, other.rating);
-    }
-
-    public static void main(String[] args) {
-        // Create a list of movies
-        List<Movies> movieList = new ArrayList<>();
-        movieList.add(new Movies("Inception", 8.8, 2010));
-        movieList.add(new Movies("The Dark Knight", 9.0, 2008));
-        movieList.add(new Movies("Interstellar", 8.6, 2014));
-        movieList.add(new Movies("The Matrix", 8.7, 1999));
-
-        // Sort the movies by rating using Comparable (compareTo)
-        Collections.sort(movieList);
-
-        // Print sorted list of movies
-        for (Movies movie : movieList) {
-            System.out.println(movie);
-        }
-    }
+    
 }
 
---
+++ wipro_java_project/src/com/wipro/java/collections/MoviesSort.java
@@ -0,0 +1,53 @@
+/**
+ * 
+ */
+package com.wipro.java.collections;
+import java.util.Collections;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 
+ */
+public class MoviesSort implements Comparable<MoviesSort>{
+	private Movies movie;
+
+	/**
+	 * @return the movie
+	 */
+	public Movies getMovie() {
+		return movie;
+	}
+	public MoviesSort(Movies movie) {
+		this.movie=movie;
+	}
+
+	@Override
+    public int compareTo(MoviesSort other) {
+        return Double.compare(this.movie.getRating(), other.getMovie().getRating());
+    }
+	public static void main(String[] args) {
+        // Create a list of Movies POJOs
+        List<Movies> movieList = new ArrayList<>();
+        movieList.add(new Movies("Inception", 8.8, 2010));
+        movieList.add(new Movies("The Dark Knight", 9.0, 2008));
+        movieList.add(new Movies("Interstellar", 8.6, 2014));
+        movieList.add(new Movies("The Matrix", 8.7, 1999));
+
+        // Convert Movies POJOs into MoviesComparable objects
+        List<MoviesSort> comparableMovies = new ArrayList<>();
+        for (Movies movie : movieList) {
+            comparableMovies.add(new MoviesSort(movie));
+        }
+
+        // Sort the movies by rating using Comparable
+        Collections.sort(comparableMovies);
+
+        // Print sorted list of movies
+        for (MoviesSort movieComparable : comparableMovies) {
+            System.out.println(movieComparable);
+        }
+    }
+	
+
+}
